c4ab4581ca2751340b8a26142e16f6ef3ab3ae00
Meerfolk <meerfolk@gmail.com>
2021-08-09 03:02:12 +0300

    Add error handling

--- src/init-routes.ts
+++ src/init-routes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance, RequestGenericInterface } from 'fastify';
 import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
 
 import { IContext } from './domain/context.interface';
 import { exchangeCode } from './domain/exchange-code';
-import { OauthTypesEnum } from './domain/oauth/oauth-types-enum';
+import { OauthTypesEnum } from './domain/oauth';
 
 interface IOauthRedirectRequest extends RequestGenericInterface {
     Querystring: {
@@ -15,19 +16,20 @@ interface IOauthRedirectRequest extends RequestGenericInterface {
     };
 }
 
-const addRedirectRoute =
-    (context: IContext) =>
-    (fastify: FastifyInstance) => {
-        fastify.get<IOauthRedirectRequest, unknown>(
-            '/v1/oauth-redirect/:oauthType',
-            async (request) => {
-                const exchange = exchangeCode(context)(request.params.oauthType)(request.query.code);
+const addRedirectRoute = (context: IContext) => (fastify: FastifyInstance) => {
+    fastify.get<IOauthRedirectRequest, unknown>('/v1/oauth-redirect/:oauthType', async (request) => {
+        return pipe(
+            exchangeCode(context)(request.params.oauthType)(request.query.code),
+            TE.fold(
+                () => {
+                    throw new Error('Something goes wrong');
+                },
+                (result) => async () => result,
+            ),
+        )();
+    });
 
-                return exchange();
-            }
-        );
-
-        return fastify;
-    };
+    return fastify;
+};
 
 export const initRoutes = pipe(addRedirectRoute);
